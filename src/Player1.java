4b1826e30c94f5010ac3ff4ed4540eebcc1986d2
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-16 15:05:43 +0545

    added new objects

--- src/Player1.java
+++ src/Player1.java
@@ -3,6 +3,10 @@ public class Player1 {
     private String weapon;
     private int health;
 
+    public Player1(){
+
+    }
+
     public Player1(String name, String weapon, int health) {
         this.name = name;
         this.weapon = weapon;
--- src/com/Simplegame/Player1.java
+++ src/com/Simplegame/Player1.java
@@ -1,2 +1,15 @@
-package com.Simplegame;public class Player1 {
+package com.Simplegame;
+
+public class Player1 {
+
+    public String weapon;
+    public int health;
+    public String name;
+
+    public String healthRemaining() {
+        return null;
+    }
+
+    public void loseHealth(int damage) {
+    }
 }
