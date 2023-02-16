3606bf2c00a5b1e876e9b1363e7cb7ce4de8c131
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-16 15:04:58 +0545

    player status

--- src/com/Simplegame/Main.java
+++ src/com/Simplegame/Main.java
@@ -1,4 +1,13 @@
+package com.Simplegame;
 public class Main {
     public static void main(String[] args) {
+        Player1 player = new Player1();
+        player.name = "Tim";
+        player.health = 20;
+        player.weapon = "Sword";
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+        
     }
 }
